0d2c8ee36d7eab596e2fb3de9abe98c45f5f4b0f
5tronciy <romangolchuk@gmail.com>
2020-10-06 20:11:52 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import produce from "immer";
+import React, { useState } from "react";
+import "./App.css";
+​
+const initialState = {
+  userProfile: {
+    id: "1",
+    nickName: "Alfons",
+    avatar: "img",
+  },
+  chats: {
+    777: {
+      title: "nexta",
+      id: "777",
+      messages: [{ time: "2020-09-30T20:00", from: "nexta", text: "Hello" }],
+      draft: "",
+    },
+    888: {
+      title: "tutBY",
+      id: "888",
+      messages: [{ time: "2020-08-30T24:00", from: "tutBY", text: "Hi" }],
+      draft: "",
+    },
+  },
+​
+  currentChatId: null,
+};
+​
+const months = [
+  "январь",
+  "февраль",
+  "март",
+  "апрель",
+  "май",
+  "июнь",
+  "июль",
+  "август",
+  "сентябрь",
+  "октябрь",
+  "ноябрь",
+  "декабрь",
+];
+​
+const getTime = (isoTime) => {
+  const time = new Date(isoTime);
+​
+  return `${time.getHours()}:${time
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")} ${time.getDate()} ${months[time.getMonth()]}`;
+};
+​
+const Chat = ({ currentChat, onDraftChange, onSendMessage }) => {
+  const onChangeHandle = (event) => {
+    const text = event.currentTarget.value;
+​
+    onDraftChange(text);
+  };
+​
+  const onSendMessageHandler = (event) => {
+    event.preventDefault();
+    onSendMessage();
+  };
+​
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{currentChat.title}</div>
+      <div>
+        {currentChat.messages.map((message) => (
+          <div>
+            {message.text} {getTime(message.time)}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={onSendMessageHandler}>
+        <input
+          type="text"
+          value={currentChat.draft}
+          onChange={onChangeHandle}
+        />
+        <button type="submit">send</button>
+      </form>
     </div>
   );
-}
-
-export default App;
+};
+​
+const ChatList = ({chats, onViewChat}) => {
+​
+  return (
+    <div>
+      {Object.values(chats).map((chat) => (
+        <div onClick={()=>{onViewChat(chat.id)}}>{chat.title}</div>
+      ))}
+    </div>
+  );
+};
+​
+const Router = () => {
+  const [state, setState] = useState(initialState);
+  const currentChat = state.chats[state.currentChatId];
+​
+  const onDraftChange = (text) => {
+    const newState = produce(state, (draftState) => {
+      draftState.chats[state.currentChatId].draft = text;
+    });
+    setState(newState);
+  };
+​
+  const onSendMessage = () =>{
+    const newState = produce(state, (draftState) => {
+      draftState.chats[state.currentChatId].messages.push({
+        text: draftState.chats[state.currentChatId].draft,
+        time: new Date().toISOString(),
+        from: draftState.userProfile.id,
+      });
+      draftState.chats[state.currentChatId].draft = "";
+    });
+    setState(newState);
+  }
+​
+  const onViewChat = (chatId) => {
+    const newState = {...state, currentChatId:chatId}
+    setState(newState);
+  }
+  
+  return state.currentChatId !== null ? (
+    <Chat currentChat={currentChat} onDraftChange={onDraftChange} onSendMessage={onSendMessage}/>
+  ) : (
+    <ChatList chats={state.chats} onViewChat={onViewChat}/>
+  );
+};
+export default Router;
