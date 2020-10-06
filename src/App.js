9aafc5b3b240d55f2a36692f8937d540710dc6fe
5tronciy <romangolchuk@gmail.com>
2020-10-06 22:02:33 +0300

    add backButton and addChat

--- src/App.js
+++ src/App.js
@@ -22,7 +22,10 @@ const initialState = {
       draft: "",
     },
   },
-  currentChatId: null,
+  currentPage: {
+    type: "chat",
+    currentChatId: "777",
+  },
 };
 
 const months = [
@@ -39,7 +42,9 @@ const months = [
   "ноябрь",
   "декабрь",
 ];
-
+const generateId = () => {
+  return Math.random().toString();
+};
 const getTime = (isoTime) => {
   const time = new Date(isoTime);
   return `${time.getHours()}:${time
@@ -48,7 +53,7 @@ const getTime = (isoTime) => {
     .padStart(2, "0")} ${time.getDate()} ${months[time.getMonth()]}`;
 };
 
-const Chat = ({ currentChat, onDraftChange, onSendMessage }) => {
+const Chat = ({ currentChat, onDraftChange, onSendMessage, onBack }) => {
   const onChangeHandle = (event) => {
     const text = event.currentTarget.value;
     onDraftChange(text);
@@ -61,10 +66,11 @@ const Chat = ({ currentChat, onDraftChange, onSendMessage }) => {
 
   return (
     <div className="App">
+      <button onClick={onBack} type="button">{`<`}</button>
       <div>{currentChat.title}</div>
       <div>
         {currentChat.messages.map((message) => (
-          <div>
+          <div key={message.time + message.from}>
             {message.text} {getTime(message.time)}
           </div>
         ))}
@@ -81,11 +87,41 @@ const Chat = ({ currentChat, onDraftChange, onSendMessage }) => {
   );
 };
 
-const ChatList = ({chats, onViewChat}) => {
+const AddChat = ({ onBack, onAddChat }) => {
+  const [chatName, setChatName] = useState("");
+  const onChangeHandler = (event) => {
+    const name = event.currentTarget.value;
+    setChatName(name);
+  };
+
+  const onAddChatHandler = (event) => {
+    event.preventDefault();
+    onAddChat(chatName);
+  };
+  return (
+    <>
+      <button onClick={onBack} type="button">{`<`}</button>
+      <form onSubmit={onAddChatHandler}>
+        <input type="text" value={chatName} onChange={onChangeHandler} />
+        <button type="submit">Add</button>
+      </form>
+    </>
+  );
+};
+
+const ChatList = ({ chats, onViewChat, onAddChat }) => {
   return (
     <div>
+      <button onClick={onAddChat}>Add chat</button>
       {Object.values(chats).map((chat) => (
-        <div onClick={()=>{onViewChat(chat.id)}}>{chat.title}</div>
+        <div
+          onClick={() => {
+            onViewChat(chat.id);
+          }}
+          key={chat.id}
+        >
+          {chat.title}
+        </div>
       ))}
     </div>
   );
@@ -93,32 +129,71 @@ const ChatList = ({chats, onViewChat}) => {
 
 const Router = () => {
   const [state, setState] = useState(initialState);
-  const currentChat = state.chats[state.currentChatId];
+  const currentChat = state.chats[state.currentPage.currentChatId];
   const onDraftChange = (text) => {
     const newState = produce(state, (draftState) => {
-      draftState.chats[state.currentChatId].draft = text;
+      draftState.chats[state.currentPage.currentChatId].draft = text;
     });
     setState(newState);
   };
-  const onSendMessage = () =>{
+  const onSendMessage = () => {
     const newState = produce(state, (draftState) => {
-      draftState.chats[state.currentChatId].messages.push({
-        text: draftState.chats[state.currentChatId].draft,
+      draftState.chats[state.currentPage.currentChatId].messages.push({
+        text: draftState.chats[state.currentPage.currentChatId].draft,
         time: new Date().toISOString(),
         from: draftState.userProfile.id,
       });
-      draftState.chats[state.currentChatId].draft = "";
+      draftState.chats[state.currentPage.currentChatId].draft = "";
     });
     setState(newState);
-  }
+  };
   const onViewChat = (chatId) => {
-    const newState = {...state, currentChatId:chatId}
+    const newState = {
+      ...state,
+      currentPage: { currentChatId: chatId, type: "chat" },
+    };
     setState(newState);
-  }
-  return state.currentChatId !== null ? (
-    <Chat currentChat={currentChat} onDraftChange={onDraftChange} onSendMessage={onSendMessage}/>
+  };
+  const onBack = () => {
+    const newState = { ...state, currentPage: { type: "chatList" } };
+    setState(newState);
+  };
+
+  const onGoToAddChat = () => {
+    const newState = { ...state, currentPage: { type: "addChat" } };
+    setState(newState);
+  };
+
+  const onAddChat = (chatName) => {
+    const id = generateId();
+    const newState = {
+      ...state,
+      chats: {
+        ...state.chats,
+        [id]: { title: chatName, id: id, messages: [], draft: "" },
+      },
+      currentPage: { type: "chat", currentChatId: id },
+    };
+    setState(newState);
+  };
+
+  return state.currentPage.type === "chat" ? (
+    <Chat
+      currentChat={currentChat}
+      onDraftChange={onDraftChange}
+      onSendMessage={onSendMessage}
+      onBack={onBack}
+    />
+  ) : state.currentPage.type === "chatList" ? (
+    <ChatList
+      chats={state.chats}
+      onViewChat={onViewChat}
+      onAddChat={onGoToAddChat}
+    />
+  ) : state.currentPage.type === "addChat" ? (
+    <AddChat onBack={onBack} onAddChat={onAddChat} />
   ) : (
-    <ChatList chats={state.chats} onViewChat={onViewChat}/>
+    <div>Page Not Found</div>
   );
 };
 
