3b5b3ac4d6dd6d9d36ea599597814d957b184306
Stronciy <gorovin-ne@ya.ru>
2020-10-14 09:52:01 +0300

    replace function AddChat with class AddChat

--- src/AddChat.js
+++ src/AddChat.js
@@ -1,63 +1,68 @@
 import React, { useState } from "react";
 import "./AddChat.css";
 
-export const AddChat = ({ onAddChat }) => {
-  const [chatName, setChatName] = useState("");
+export class AddChat extends React.Component {
+  render({ onAddChat }) {
+    const [chatName, setChatName] = useState("");
 
-  const onChangeHandler = (event) => {
-    const name = event.currentTarget.value;
-    setChatName(name);
-  };
+    const onChangeHandler = (event) => {
+      const name = event.currentTarget.value;
+      setChatName(name);
+    };
 
-  const onAddChatHandler = (event) => {
-    event.preventDefault();
-    onAddChat(chatName);
-  };
+    const onAddChatHandler = (event) => {
+      event.preventDefault();
+      onAddChat(chatName);
+    };
 
-  const onClose = () => {};
+    const onClose = () => {};
 
-  return (
-    <React.Fragment>
-      <div className="modal">
-        <div className="addChat">
-          <div className="addChat-body">
-            <div className="addChat-title">
-              <h5>Create New Chat</h5>
-              <button type="button" className="close" onClick={onClose}>
-                <span aria-hidden="true">×</span>
-              </button>
-            </div>
-            <div className="addChat-description">
-              <form onSubmit={onAddChatHandler}>
-                <div className="form-group">
-                  <label for="addChatName-input">Chat Name</label>
-                  <input
-                    id="addChatName-input"
-                    placeholder="Enter Chat Name"
-                    type="text"
-                    value={chatName}
-                    onChange={onChangeHandler}
-                    className="form-control"
-                  />
-                </div>
-                <div className="form-group">
-                  <label for="addChatAvatar-input">Avatar</label>
-                  <input
-                    id="addChatAvatar-input"
-                    type="file"
-                    className="form-control"
-                  />
-                </div>
-              </form>
-            </div>
-            <div className="modalFooter">
-              <button onClick={onAddChatHandler} className="createChat-button">
-                Create Chat
-              </button>
+    return (
+      <React.Fragment>
+        <div className="modal">
+          <div className="addChat">
+            <div className="addChat-body">
+              <div className="addChat-title">
+                <h5>Create New Chat</h5>
+                <button type="button" className="close" onClick={onClose}>
+                  <span aria-hidden="true">×</span>
+                </button>
+              </div>
+              <div className="addChat-description">
+                <form onSubmit={onAddChatHandler}>
+                  <div className="form-group">
+                    <label for="addChatName-input">Chat Name</label>
+                    <input
+                      id="addChatName-input"
+                      placeholder="Enter Chat Name"
+                      type="text"
+                      value={chatName}
+                      onChange={onChangeHandler}
+                      className="form-control"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="addChatAvatar-input">Avatar</label>
+                    <input
+                      id="addChatAvatar-input"
+                      type="file"
+                      className="form-control"
+                    />
+                  </div>
+                </form>
+              </div>
+              <div className="modalFooter">
+                <button
+                  onClick={onAddChatHandler}
+                  className="createChat-button"
+                >
+                  Create Chat
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </React.Fragment>
-  );
-};
+      </React.Fragment>
+    );
+  }
+}
