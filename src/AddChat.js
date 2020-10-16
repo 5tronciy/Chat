8cc19bebf9007b85f24e007f1bc2a4808ea74ffd
5tronciy <62604867+5tronciy@users.noreply.github.com>
2020-10-16 15:42:21 +0200

    Update AddChat.js
    back to function & fix errors

--- src/AddChat.js
+++ src/AddChat.js
@@ -1,68 +1,58 @@
 import React, { useState } from "react";
 import "./AddChat.css";
 
-export class AddChat extends React.Component {
-  render() {
-    const [chatName, setChatName] = useState("");
+export const AddChat = ({ onAddChat }) => {
+  const [chatName, setChatName] = useState("");
 
-    const onChangeHandler = (event) => {
-      const name = event.currentTarget.value;
-      setChatName(name);
-    };
+  const onChangeHandler = (event) => {
+    const name = event.currentTarget.value;
+    setChatName(name);
+  };
 
-    const onAddChatHandler = (event) => {
-      event.preventDefault();
-      this.props.onAddChat(chatName);
-    };
+  const onAddChatHandler = (event) => {
+    event.preventDefault();
+    onAddChat(chatName);
+  };
 
-    const onClose = () => {};
+  const onClose = () => {};
 
-    return (
-      <React.Fragment>
-        <div className="modal">
-          <div className="addChat">
-            <div className="addChat-body">
-              <div className="addChat-title">
-                <h5>Create New Chat</h5>
-                <button type="button" className="close" onClick={onClose}>
-                  <span aria-hidden="true">×</span>
-                </button>
-              </div>
-              <div className="addChat-description">
-                <form onSubmit={onAddChatHandler}>
-                  <div className="form-group">
-                    <label for="addChatName-input">Chat Name</label>
-                    <input
-                      id="addChatName-input"
-                      placeholder="Enter Chat Name"
-                      type="text"
-                      value={chatName}
-                      onChange={onChangeHandler}
-                      className="form-control"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label for="addChatAvatar-input">Avatar</label>
-                    <input
-                      id="addChatAvatar-input"
-                      type="file"
-                      className="form-control"
-                    />
-                  </div>
-                </form>
-              </div>
-              <div className="modalFooter">
-                <button
-                  onClick={onAddChatHandler}
-                  className="createChat-button"
-                >
-                  Create Chat
-                </button>
-              </div>
+  return (
+    <React.Fragment>
+      <div className="modal">
+        <div className="addChat">
+          <div className="addChat-body">
+            <div className="addChat-title">
+              <h5>Create New Chat</h5>
+              <button type="button" className="close" onClick={onClose}>
+                <span aria-hidden="true">×</span>
+              </button>
+            </div>
+            <div className="addChat-description">
+              <form onSubmit={onAddChatHandler}>
+                <div className="form-group">
+                  <label>Chat Name</label>
+                  <input
+                    placeholder="Enter Chat Name"
+                    type="text"
+                    value={chatName}
+                    onChange={onChangeHandler}
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Avatar</label>
+                  <input type="file" className="form-control" />
+                </div>
+              </form>
+            </div>
+            <div className="modalFooter">
+              <button onClick={onAddChatHandler} className="createChat-button">
+                Create Chat
+              </button>
             </div>
           </div>
         </div>
-      </React.Fragment>
-    );
-  }
-}
+      </div>
+    </React.Fragment>
+  );
+};
