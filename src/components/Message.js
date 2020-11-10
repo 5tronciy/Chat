18dc7039622788ee41b96fb1161f1d4fa44dce9c
Stronciy <gorovin-ne@ya.ru>
2020-11-10 16:38:57 +0300

    install styled components

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { getTime } from "./App";
 import { useSelector } from "react-redux";
+import styles from "../styles/Message.css";
 
 const Message = ({ message }) => {
   const currentUser = useSelector((state) => state.userProfile);
   const currentChat = useSelector((state) => state.chats[message.from]);
   return (
     <li className={message.from === currentUser.id ? "right" : ""}>
-      <div className="message">
-        <div className="chat-avatar">
+      <div className={styles.message}>
+        <div className={styles.avatar}>
           <img
             src={
               process.env.PUBLIC_URL +
@@ -18,17 +19,17 @@ const Message = ({ message }) => {
                 : currentChat.avatar)
             }
             alt=""
-            className="avatar"
+            className={styles.avatarImage}
           />
         </div>
-        <div className="message-content">
-          <div className="chat__message-textWrapper">
-            <div className="chat__message-text-content">
+        <div className={styles.messageContent}>
+          <div className={styles.messageTextWrapper}>
+            <div className={styles.messageTextContent}>
               <div>{message.text} </div>
-              <div className="chat__message-time">{getTime(message.time)}</div>
+              <div className={styles.messageTime}>{getTime(message.time)}</div>
             </div>
           </div>
-          <div className="conversation-name">
+          <div className={styles.conversationName}>
             {message.from === currentUser.id
               ? currentUser.nickName
               : currentChat.title}
