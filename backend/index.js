1aa267404a349d873a4b53a63963ce4c07fbb473
Stronciy <gorovin-ne@ya.ru>
2020-11-17 16:42:50 +0300

    koa cascade template

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,39 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  req.url === "/chats"
-    ? res.end(JSON.stringify(getChats(1)))
-    : res.end("Hello world");
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   req.url === "/chats"
+//     ? res.end(JSON.stringify(getChats(1)))
+//     : res.end("Hello world");
+// });
+// server.listen(3000);
+
+const Koa = require("koa");
+const app = new Koa();
+
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get("X-Response-Time");
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 });
-server.listen(3000);
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", `${ms}ms`);
+});
+
+// response
+
+app.use(async (ctx) => {
+  ctx.body = "Hello, World!";
+});
+
+app.listen(3000);
 
 let state = {
   users: {
