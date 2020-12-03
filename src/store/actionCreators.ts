baa9a4121de6eff0acc718c54c2c5b6fd9181dee
Stronciy <gorovin-ne@ya.ru>
2020-12-03 10:05:52 +0300

    refactor (Action Types instead of interfaces)

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -1,85 +1,40 @@
-interface ChatNameChangeInterface{
-  type:String,
-  title:String
-}
-export function chatNameChange (value:String):ChatNameChangeInterface {
-  return { type: "CHAT_NAME_CHANGE", title: value };
-}
-interface CreateChatInterface{
-  type:String,
-  id:Number|String,
-  title:String,
-  avatar:String
-}
-export function createChat(id:Number|String, title:String, avatar:String):CreateChatInterface {
-  return { type: "CREATE_CHAT", id: id, title: title, avatar: avatar };
-}
-interface DraftChangeInterface{
-  type:String,
-  message:String,
-  currentChatId:Number|String
-  
-}
-export function draftChange(value:String, currentChatId:Number|String):DraftChangeInterface {
-  return {
+export const chatNameChange = (value:String)=>(<const> {
+   type: "CHAT_NAME_CHANGE", title: value });
+
+export const createChat = (id:Number|String, title:String, avatar:String)=>(<const> {
+  type: "CREATE_CHAT", id: id, title: title, avatar: avatar });
+
+export const draftChange=(value:String, currentChatId:Number|String)=> (<const> {
     type: "DRAFT_CHANGE",
     message: value,
     currentChatId: currentChatId
-  };
-}
-interface LoadAvatarInterface{
-  type:String,
-  avatar:String
-}
-export function loadAvatar(value:String):LoadAvatarInterface {
-  return {
+  });
+
+export const loadAvatar=(value:String)=>(<const> {
     type: "LOAD_AVATAR",
     avatar: value
-  };
-}
-interface SendMessageInterface{
-  type:String,
-  currentChatId:Number|String,
-  userProfileId:Number|String
-}
-export function sendMessage(currentChatId:Number|String, userProfileId:Number|String):SendMessageInterface {
-  return {
-    type: "SEND_MESSAGE",
+  });
+
+export const sendMessage=(currentChatId:Number|String, userProfileId:Number|String)=> (<const>{
+      type: "SEND_MESSAGE",
     currentChatId: currentChatId,
     userProfileId: userProfileId
-  };
-}
-interface LoginUserInterface{
-  type:String,
-  userProfile:Object
-}
-export function loginUser(value:String):LoginUserInterface {
-  return {
+  });
+
+export const loginUser=(value:String)=>(<const> {
     type: "LOGIN_USER",
     userProfile: value
-  };
-}
-interface ShowModalAddChatInterface{
-  type:String,
-  modal:Boolean
-}
-export function showModalAddChat(value:Boolean):ShowModalAddChatInterface {
-  return {
+  });
+
+export const showModalAddChat=(value:Boolean)=> (<const> {
     type: "SHOW_MODAL_ADD_CHAT",
     modal: value
-  };
-}
-interface ViewChatInterface{
-  type:String,
-  chatId:Number|String
-}
-export function viewChat(chatId:Number|String):ViewChatInterface {
-  return { type: "VIEW_CHAT", chatId: chatId };
-}
-interface FetchChatsInterface{
-  type:String,
-  chats:Object
-}
-export function fetchChats(chats:Object):FetchChatsInterface {
-  return { type: "FETCH_CHATS", chats: chats };
-}
+  });
+
+export const viewChat=(chatId:Number|String)=>(<const> {
+  type: "VIEW_CHAT", chatId: chatId });
+
+export const fetchChats=(chats:Object)=>(<const> {
+  type: "FETCH_CHATS", chats: chats });
+
+export type ActionTypes = ReturnType<typeof chatNameChange>| ReturnType<typeof createChat>| ReturnType<typeof draftChange>| ReturnType<typeof loadAvatar>| ReturnType<typeof sendMessage>| ReturnType<typeof loginUser>| ReturnType<typeof showModalAddChat>| ReturnType<typeof viewChat>| ReturnType<typeof fetchChats>
