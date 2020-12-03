5bf10435add176eed6805b04867b3856322a280e
Stronciy <gorovin-ne@ya.ru>
2020-12-03 11:41:43 +0300

    add MessageInterface

---
+++ src/store/selectors.ts
@@ -0,0 +1,20 @@
+export const getUserId = (state:any) => state.userProfile.id;
+
+export const getModalAddChatState = (state:any) => state.modal;
+
+export const getCurrentChatId = (state:any) => state.currentChat.currentChatId;
+
+export const getCurrentChat = (state:any) =>
+  state.chats[state.currentChat.currentChatId];
+
+export const getChats = (state:any) => state.chats;
+
+export const getCurrentUser = (state:any) => state.userProfile;
+interface MessageInterface {
+from:String;
+time: String;
+text: String
+}
+export function getChatOfMessage(message:MessageInterface) {
+  return (state:any) => state.chats[Number(message.from)];
+}
